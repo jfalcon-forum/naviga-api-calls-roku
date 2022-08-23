4d7e352fbb2a0db71e3ef75b2769f4f1e6f6f470
Jordan Falcon <jfalcon@forumcomm.com>
2022-08-23 10:28:51 -0500

    Changed the forEach loop to a for loop to allow break statement to bounch out of loop when a livestream subscription is present

--- index-navSub.js
+++ index-navSub.js
@@ -2,77 +2,96 @@ const { getNavigaSubscriptions } = require('./naviga-subscriptions');
 
 const navigaCall = async (id) => {
   if (id === null || id === "" || id === undefined) {
-    return {
-      "status": 204,
-      "message": "Invalid ID"
-    };
+    return [
+      {
+        "status": 204,
+        "message": "Invalid ID"
+      }
+    ];
   }
   const navResponse = await getNavigaSubscriptions(id);
   if (navResponse.Errors.length > 1) {
-    return {
-      "status": 203,
-      "message": navResponse.Errors[0].Message
-    };
+    return [
+      {
+        "status": 203,
+        "message": navResponse.Errors[0].Message
+      }
+    ];
   }
   let response;
   // check for Owned Subscriptions
   if (navResponse.Result.OwnedSubscriptions !== null && navResponse.Result.OwnedSubscriptions.length > 0) {
     let startDate, endDate;
-    navResponse.Result.OwnedSubscriptions.forEach(subscription => {
-      if (subscription.Status === "L" && subscription.BaseProductPaperCode === "SLS") {
-        startDate = new Date(subscription.StartDate).getTime();
-        endDate = new Date(subscription.ExpirationDate).getTime();
-        response = {
-          "status": 200,
-          "product_id": "SLS",
-          "starts_at": startDate,
-          "ends_at": endDate
-        };
+    for (let i = 0; i < navResponse.Result.OwnedSubscriptions.length; i++) {
+      if (navResponse.Result.OwnedSubscriptions[i].Status === "L" && navResponse.Result.OwnedSubscriptions[i].BaseProductPaperCode === "SLS") {
+        startDate = Math.floor(new Date(navResponse.Result.OwnedSubscriptions[i].StartDate).getTime() / 1000);
+        endDate = Math.floor(new Date(navResponse.Result.OwnedSubscriptions[i].ExpirationDate).getTime() / 1000);
+        response = [
+          {
+            "status": 200,
+            "product_id": "SLS",
+            "starts_at": startDate,
+            "ends_at": endDate
+          }
+        ];
+        break;
       } else {
-        response = {
-          "status": 203,
-          "message": "No active livestream subscriptions for this user"
-        };
+        response = [
+          {
+            "status": 203,
+            "message": "No active Livestream Subscriptions for this user"
+          }
+        ]; 
       }
-    });
+    }
   } 
   // Check for Guest Subscriptions
   if (navResponse.Result.GuestSubscriptions !== null && navResponse.Result.GuestSubscriptions.length > 0) {
     let startDate, endDate;
-    navResponse.Result.GuestSubscriptions.forEach(subscription => {
-      if (subscription.Status === "L" && subscription.BaseProductPaperCode === "SLS") {
-        startDate = new Date(subscription.StartDate).getTime();
-        endDate = new Date(subscription.ExpirationDate).getTime();
-        response = {
-          "status": 200,
-          "product_id": "SLS",
-          "starts_at": startDate,
-          "ends_at": endDate
-        };
-      } 
-      else {
-        response = {
-          "status": 203,
-          "message": "No active livestream subscriptions for this user"
-        };
+    for (let i = 0; i < navResponse.Result.GuestSubscriptions.length; i++) {
+      if (navResponse.Result.GuestSubscriptions[i].Status === "L" && navResponse.Result.GuestSubscriptions[i].BaseProductPaperCode === "SLS") {
+        startDate = Math.floor(new Date(navResponse.Result.GuestSubscriptions[i].StartDate).getTime() / 1000);
+        endDate = Math.floor(new Date(navResponse.Result.GuestSubscriptions[i].ExpirationDate).getTime() / 1000);
+        response = [
+          {
+            "status": 200,
+            "product_id": "SLS",
+            "starts_at": startDate,
+            "ends_at": endDate
+          }
+        ];
+        break;
+      } else {
+        response = [
+          {
+            "status": 203,
+            "message": "No active Livestream Subscriptions for this user"
+          }
+        ]; 
       }
-    })
+    }
   }
   if (!response) {
-    response = {
-      "status": 203,
-      "message": "User does not exist or does not have any subscriptions."
-    };
+    response = [
+      {
+        "status": 203,
+        "message": "User does not exist or does not have any subscriptions."
+      }
+    ];
   }
   return response;
 };
 
+navigaCall('auth0|62fab2b0c11aa2e79d548ae1').then(console.log)
+
 exports.handler = async (event, context) => {
   if (event.query === null || event.query === "" || event.query === undefined) {
-    return {
-      "status": 204,
-      "message": "Invalid ID"
-    };
+    return [
+      {
+        "status": 204,
+        "message": "Invalid ID"
+      }
+    ];
   }
   let authId = decodeURIComponent(event.query.consumer_id);
   const response = await navigaCall(authId);
