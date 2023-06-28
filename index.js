216cdc22bdfbbbaaf68e19868fde91e0db537fa3
Jordan Falcon <jfalcon@forumcomm.com>
2023-06-28 13:52:24 -0500

    updating github to match lambda

--- index.js
+++ index.js
@@ -1,28 +1,96 @@
-const { getLivestreamAccess} = require('./naviga-livestream');
+const jwt_decode = require('jwt-decode');
+const { getNavigaSubscriptions } = require('./naviga-subscriptions');
 
 const navigaCall = async (id) => {
   if (id === null || id === "" || id === undefined) {
     return {
-      "status": 204,
-      "message": "Invalid ID"
-    }
+        "status": 401,
+        "message": "Invalid ID"
+      }
   }
-  const navResponse = await getLivestreamAccess(id)
+  const navResponse = await getNavigaSubscriptions(id);
   if (navResponse.Errors.length > 1) {
     return {
-      "status": 203,
-      "message": navResponse.Errors[0].Message
+        "status": 401,
+        "message": navResponse.Errors[0].Message
+      }
+  }
+  let response;
+  // check for Owned Subscriptions
+  if (navResponse.Result.OwnedSubscriptions !== null && navResponse.Result.OwnedSubscriptions.length > 0) {
+    let startDate, endDate;
+    for (let i = 0; i < navResponse.Result.OwnedSubscriptions.length; i++) {
+      if (navResponse.Result.OwnedSubscriptions[i].Status === "L" && navResponse.Result.OwnedSubscriptions[i].BaseProductPaperCode === "SLS") {
+        startDate = Math.floor(new Date(navResponse.Result.OwnedSubscriptions[i].StartDate).getTime() / 1000);
+        endDate = Math.floor(new Date(navResponse.Result.OwnedSubscriptions[i].ExpirationDate).getTime() / 1000);
+        response = {
+            "status": 200,
+            "product_id": "SLS",
+            "starts_at": startDate,
+            "ends_at": endDate
+          }
+        break;
+      } else {
+        response =
+          {
+            "status": 401,
+            "message": "No active Livestream Subscriptions for this user"
+          }
+      }
+    }
+  } 
+  // Check for Guest Subscriptions
+  if (navResponse.Result.GuestSubscriptions !== null && navResponse.Result.GuestSubscriptions.length > 0) {
+    let startDate, endDate;
+    for (let i = 0; i < navResponse.Result.GuestSubscriptions.length; i++) {
+      if (navResponse.Result.GuestSubscriptions[i].Status === "L" && navResponse.Result.GuestSubscriptions[i].BaseProductPaperCode === "SLS") {
+        startDate = Math.floor(new Date(navResponse.Result.GuestSubscriptions[i].StartDate).getTime() / 1000);
+        endDate = Math.floor(new Date(navResponse.Result.GuestSubscriptions[i].ExpirationDate).getTime() / 1000);
+        response =
+          {
+            "status": 200,
+            "product_id": "SLS",
+            "starts_at": startDate,
+            "ends_at": endDate
+          }
+        break;
+      } else {
+        response =
+          {
+            "status": 401,
+            "message": "No active Livestream Subscriptions for this user"
+          }
+      }
     }
   }
-  if (navResponse.Result.IsAuthorized) {
-    return {
-      "status": 200,
-      ...navResponse,
-    } 
+  if (!response) {
+    response = {
+      "status": 401,
+      "message": "User does not exist or does not have any subscriptions."
+    }
   }
-}
+  return response;
+};
 
-exports.handler = async (event, context) => {
-  const response = await navigaCall(event.id);
+exports.handler = async (event, context, callback) => {
+  if (event.query === null || event.query === "" || event.query === undefined) {
+    return {
+        "status": 401,
+        "message": "Invalid ID"
+      }
+  }
+  let authId;
+  if (event.query.ctx.length > 5) {
+    let buff = Buffer.from(event.query.ctx, 'base64');
+    let text = buff.toString();
+    let decoded = await jwt_decode(text);
+    authId = decoded.sub;
+  } else {
+    return {
+      "status": 401,
+      "message": "Invalid ctx value."
+    }
+  }
+  const response = await navigaCall(authId);
   return response;
 };
